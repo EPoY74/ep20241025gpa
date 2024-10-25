16055c33dd73f66c30f39a5c0ee5a6f6dfd0dc34
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-25 15:12:18 +0500

    init перве переменные

--- Program.cs
+++ Program.cs
@@ -5,3 +5,16 @@
 * https://learn.microsoft.com/ru-ru/training/modules/guided-project-calculate-final-gpa/2-prepare
 * Дата проекта: 25.10.2024
 */
+
+string studentName = "Sophia Johnson";
+string course1Name = "English 101";
+string course2Name = "Algebra 101";
+string course3Name = "Biology 101";
+string course4Name = "Computer Science I";
+string course5Name = "Psychology 101";
+
+int course1Credit = 3;
+int course2Credit = 3;
+int course3Credit = 4;
+int course4Credit = 4;
+int course5Credit = 3;
