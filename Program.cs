8e1a3b4a1408fab309cc4ddb7ab00fb6ee68f7c6
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2024-10-25 15:11:06 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+* Автор: Евгений Петров
+* Почта: p174@mail.ru
+* Цель: Обечение c#. пишу учебное приложение для подсчета gpa
+* https://learn.microsoft.com/ru-ru/training/modules/guided-project-calculate-final-gpa/2-prepare
+* Дата проекта: 25.10.2024
+*/
